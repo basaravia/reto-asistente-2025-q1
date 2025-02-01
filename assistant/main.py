d732b93d43d635abf7415e619fdfc3ca56163682
Adrian Villacres <jvillacresp2@est.ups.edu.ec>
2025-01-31 21:08:59 -0500

    defult message on 3 endpoints

--- assistant/main.py
+++ assistant/main.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_restful import Api
 
-from assistant import AnalyzePdfAdapter, RagAdapter, ShoppingAdvisorAdapter
+from RagAdapter import RagAdapter
+from AnalyzePdfAdapter import AnalyzePdfAdapter
+from ShoppingAdvisorAdapter import ShoppingAdvisorAdapter
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,5 +13,6 @@ api.add_resource(RagAdapter, "/assistant/rag")
 api.add_resource(AnalyzePdfAdapter, "/assistant/analyze-pdf")
 api.add_resource(ShoppingAdvisorAdapter, "/assistant/shopping-advisor")
 
-if __name__ == "__name__":
-    app.run(host="172.0.0.1", port=5000, debug=True)
+if __name__ == "__main__":
+    print("🚀 Flask API iniciando en http://127.0.0.1:5000")
+    app.run(host="127.0.0.1", port=5000, debug=True)
